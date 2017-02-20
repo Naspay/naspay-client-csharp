030a6662ecc37f7796eb1a484f5e66a9322054f8
Naspay Developer <dev@naspay.com>
2017-02-20 12:45:33 +0200

    Initial version of Naspay CSharp Client

---
+++ naspay-client-csharp/NaspayClient.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace Naspay
+{
+    public class NaspayClient
+    {
+        private const string userAgent = "naspay csharp client 0.1";
+
+        private readonly string apiKey;
+        private readonly string apiSecret;
+        private readonly string baseUrl;
+        private readonly bool debug;
+
+        private string accessToken;
+
+        public NaspayClient(string domain, string apiKey, string apiSecret, bool debug)
+        {
+            this.baseUrl = "https://" + domain;
+            this.apiKey = apiKey;
+            this.apiSecret = apiSecret;
+            this.debug = debug;
+        }
+
+        public void Authenticate()
+        {
+            string basicCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(apiKey + ":" + apiSecret));
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseUrl + "/auth/token");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            request.UserAgent = userAgent;
+            request.Headers.Add("Authorization", "Basic " + basicCredentials);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string responseBody = new StreamReader(response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            Dictionary<string, object> obj = jss.Deserialize<Dictionary<string, object>>(responseBody);
+            accessToken = (string)obj["access_token"];
+            if (debug)
+            {
+                Console.WriteLine("Got access token " + accessToken);
+            }
+        }
+
+        public Dictionary<string, object> DoApiRequest(string resource, HttpMethod method, object requestObject)
+        {
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                throw new Exception("Not authenticated");
+            }
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseUrl + "/api/v1" + resource);
+            request.Method = method.Method;
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            request.UserAgent = userAgent;
+            request.Headers.Add("Authorization", "Bearer " + accessToken);
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+
+            if (requestObject != null)
+            {
+                string json = jss.Serialize(requestObject);
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {                    
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+            }
+
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string responseBody = new StreamReader(response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
+
+            if (debug)
+            {
+                Console.WriteLine("response: " + responseBody);
+            }
+
+            return jss.Deserialize<Dictionary<string, object>>(responseBody);
+        }
+
+
+    }
+}
---
+++ naspay-client-csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naspay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string domain = "naspay.com";
+            string apiKey = "apiKey";
+            string apiSecret = "apiSecret";
+
+            NaspayClient client = new NaspayClient(domain, apiKey, apiSecret, true);
+            client.Authenticate();
+
+            object map = client.DoApiRequest("/transactions", HttpMethod.Get, null);
+
+            Console.ReadKey();
+        }
+    }
+}
